80878940f65f74fd72c0eac02310de3a359f2507
Jon <jonsobocinski@comcast.net>
2015-02-18 17:06:31 -0800

    initial db support

---
+++ js/database.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Jon on 2/18/2015.
+ * Practicing in class example for setting up database
+ * uses node.js and mongoDB
+ */
+
+
+//Set a high port number like "8888"
+/**
+ * Var express = required('express);
+ * var app = express();
+ *
+ * app.listen(8888);
+ *
+ * app.get("/", function (req,res){
+ *      res.send('Hello World');
+ *      }
+ */
+
+
