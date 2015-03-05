b0c21496e2fa3606edfc1b5a853ca79edde7db1f
Matt <moorem27@uw.edu>
2015-03-05 15:54:32 -0800

    Fixed walking animation....again.....

--- js/matt.js
+++ js/matt.js
@@ -14,8 +14,8 @@ Matt.prototype.loadAnims = function() {
     if (this.playerNumber === 1) {
         //spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse)
         this.animate = new Animate(this.spritesheet, 3000, 10, 370, 470, 0.1, 3, true, false);
-        this.PunchAnimate = new Animate(this.spritesheet, 0, 1995, 370, 480, 0.02, 4, false, false);
-        this.KickAnimate = new Animate(this.spritesheet, 0, 2480, 370, 500, 0.03, 4, false, false);
+        this.PunchAnimate = new Animate(this.spritesheet, 0, 1995, 370, 480, 0.05, 4, false, false);
+        this.KickAnimate = new Animate(this.spritesheet, 0, 2480, 370, 500, 0.1, 4, false, false);
         this.WalkAnimate = new Animate(this.spritesheet, 3000, 2000, 370, 470, 0.1, 4, true, false);
         this.BlockAnimate = new Animate(this.spritesheet, 0, 0, 370, 500, 0.01, 3, false, false);
         this.HoldBlock = new Animate(this.spritesheet, 708, 0, 370, 500, 0.1, 1, true, false);
@@ -28,7 +28,7 @@ Matt.prototype.loadAnims = function() {
     } else {
         this.animate = new Animate(this.spritesheet, 3336, 1000, 370, 480, 0.1, 2, true, true);
         this.PunchAnimate = new Animate(this.spritesheet, 1500, 1995, 370, 480, 0.05, 4, false, true);
-        this.KickAnimate = new Animate(this.spritesheet, 1520, 2480, 370, 500, 0.05, 4, false, true);
+        this.KickAnimate = new Animate(this.spritesheet, 1520, 2480, 370, 500, 0.1, 4, false, true);
         this.WalkAnimate = new Animate(this.spritesheet, 11, 2960, 370, 500, 0.1, 4, true, true);
         this.BlockAnimate = new Animate(this.spritesheet, 1800, 10, 370, 500, 0.1, 3, false, true);
         this.HoldBlock = new Animate(this.spritesheet, 2540, 10, 370, 500, 0.1, 1, true, true);
