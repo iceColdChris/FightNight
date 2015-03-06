45cec31ea892d405cbb316685a1117034bd90841
Jon Sobocinski <jonsobocinski@comcast.net>
2015-03-05 23:24:07 -0800

    fixed book crashing

--- js/GameEngine.js
+++ js/GameEngine.js
@@ -129,27 +129,19 @@ GameEngine.prototype.update = function() {
             if(!this.entities[0].amIhittable()){
                 this.entities[0].hitMeScotty(10);
                 this.books.removeFromWorld = true;
+                this.books.splice(i, 1);
 
             }
         }
         if(booksX > leftTwo && booksX < (rightTwo)){
             if(!this.entities[1].amIhittable()){
                 this.entities[1].hitMeScotty(10);
-                this.books.removeFromWorld = true;
+                this.books.splice(i, 1);
+
             }
         }
     }
-    for (var i = 0; i < this.books.length; i++) {
-        if(this.books[i].x< -10){
-            this.books.removeFromWorld = true;
-        }
-        if(this.books[i].x>3500){
-            this.books.removeFromWorld = true;
-        }
-        if (this.books[i].removeFromWorld) {
-            this.books.splice(i, 1);
-        }
-    }
+
 };
 GameEngine.prototype.loop = function() {
     this.clockTick = this.timer.tick();
