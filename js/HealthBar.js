fd185e4a3480533aa89555732be232b451bfe710
Matt <moorem27@uw.edu>
2015-03-07 00:59:19 -0800

    Changed the code to where the names center under the health bar properly.

--- js/HealthBar.js
+++ js/HealthBar.js
@@ -8,7 +8,7 @@ function HealthBar(game, x, y, maxHealth, height, maxWidth, name) {
     this.y = y;
     this.maxHealth = maxHealth;
     this.height = height;
-    this.maxWidth = maxWidth;
+    this.maxWidth = maxWidth; //500
     this.health = this.maxHealth;
     this.name = name;
 }
@@ -16,21 +16,24 @@ HealthBar.prototype.setHealth = function(health) {
         this.health = health;
 };
 
+
 HealthBar.prototype.draw = function() {
+    var nameLength = this.name.length * 50;
     this.ctx.font = "100px Arial";
     this.ctx.fillStyle = "blue";
-    this.ctx.fillText(this.name, this.x + 120, this.y + 150);
+    this.ctx.fillText(this.name, (this.x + this.maxWidth/2) - (nameLength/2), this.y + 150);
     this.ctx.beginPath();
     this.ctx.rect(this.x, this.y, this.maxWidth, this.height);
     this.ctx.fillStyle = "black";
     this.ctx.fill();
     this.ctx.closePath();
+
     if (this.health > 0) {
-    this.ctx.beginPath();
-    var width = this.maxWidth * this.health / this.maxHealth;
-    this.ctx.rect(this.x, this.y, width, this.height);
-    this.ctx.fillStyle = "red";
-    this.ctx.fill();
-    this.ctx.closePath();
-}
+        this.ctx.beginPath();
+        var width = this.maxWidth * this.health / this.maxHealth;
+        this.ctx.rect(this.x, this.y, width, this.height);
+        this.ctx.fillStyle = "red";
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
 }
